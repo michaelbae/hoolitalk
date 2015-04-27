b62efeeb2ef6734df4b003148d0eaf383722993a
michael.bae <michaelbaejunyoung@gmail.com>
2015-04-27 15:29:05 -0700

    refactoring server_ver2

--- server-ver2.js
+++ server-ver2.js
@@ -19,39 +19,46 @@ CL = new Mongo.Collection("fixtures_cl");
 
 Meteor.startup(function () {
     if (Meteor.isServer){
+		//once in 24hr
         Meteor.call("updateDB");
+		Meteor.call("adminYak");
     }
 });
-
-// API call for league
 
 if (Meteor.isServer){
     Meteor.methods({
         
+		//API call for league
         getLeague: function () {
             return Meteor.http.call("GET", "http://api.football-data.org/alpha/soccerseasons/", {headers: {"X-Auth-Token": "a87a67fb2885496bb1a5274e1eb79236"}});
         },
+		
+		//API call for specific league's season worth fixtures
         getFixtures: function (leagueId) {
         	return Meteor.http.call("GET", "http://api.football-data.org/alpha/soccerseasons/" + leagueId + "/fixtures/", {headers: {"X-Auth-Token": "a87a67fb2885496bb1a5274e1eb79236"}});
         },
 
+		//parsing data
         parse: function(data) {
             var json = EJSON.stringify(data);
             return EJSON.parse(json);
         },
 
+		//update entire mongoDB
         updateDB: function(){
             var leagueData = Meteor.call("getLeague");
             var leagueJSON = Meteor.call("parse", leagueData);
             ListOfLeagues to be updated = Meteor.call("updateLeague", leagueJSON);
 
-            for (i=0; i<ListOfLeagues.length; i++){
-                Meteor.call("updateFixture", ListOfLeagues[i])
+            for (i=0; i<ListOfLeaguesId.length; i++){
+				var fixturesData = Meteor.call("getFixtures", ListOfLeaguesId[i]);
+				var fixturesJSON = Meteor.call("parse", fixturesData)
+                Meteor.call("updateFixture", ListOfLeagues[i], fixturesJSON)
             }
-        }
+        },
 
+		//update information about one league/competition in mongoDB AND create a queue of leagues that needs to update its fixtures
         updateLeague: function(league){  
-
             new list
 
             for (i=0; i<league.data.length; i++) {
@@ -64,26 +71,27 @@ if (Meteor.isServer){
                  //compare the two
                  if (league.data[i].lastUpdated != oldLeague.lastUpdated){
                     //add to the list of league that needs to be updated
-                    list.add(league.data[i].caption or id)    
+                    list.add(league.data[i].id)    
                     //replace with new League
                     Meteor.call("storeLeague", league.data[i]);
-                 } else {
-                    //do nothing on both League AND Fixtures of that league
                  }
             }  
             return list
-        }
+        },
 
-        updateFixture: function(leagueId){
+		//update a season's worth of fixtures of a specific league
+        updateFixture: function(leagueId, fixtures){
             var data = Meteor.call("getFixtures", leagueId);
             var obj = Meteor.call("parse", data);
             
+			//find corresponding mongoDB using leagueid
+			
             //replace with new League
-            Meteor.call("storeFixtures", obj.data[i], leagueId);
+            Meteor.call("storeFixtures", fixtures.data, leagueId, mongoDB);
         },   
-        
+		
+        //store information about one league/competition in mongoDB
         storeLeague: function (league) {
-
             Leagues.insert({
                 caption :  league.caption,
                 league  :   league.league,                           
@@ -94,10 +102,10 @@ if (Meteor.isServer){
                 soccerseasons : league._links
             });       
         },
-
-        storeFixtures: function (fixtures) {}
-        	
-            Fixtures.insert({
+		
+		//store a season's worth of fixtures of a specific league in mongoDB
+        storeFixtures: function (fixtures, leagueId, mongoDB) {
+            mongoDB.insert({
                 id: 164374834
         		date: obj.fixtures[i].date,
         		status: obj.fixtures[i].status,
@@ -110,8 +118,20 @@ if (Meteor.isServer){
                 fixturenumber
         	});
     	}
+		
+		adminYak: function(){
+			//look at the above 15 league's fixtures mongoDB and find the 3p3n mathches (~150)
+			
+			//run loop to create 150 yak DB collections.
+		}
     });
 }
 
+client
+1. league page - league.find()
+(pass a league's fixture DB)
+2. upcoming fixtures - fixturesDB.find(where date is p3n3)
+(pass a match DB)
+3. match's yak - matchDB.find() 
 
 
