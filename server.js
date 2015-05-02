bebb23e0d523ee0c6633a9605524656468b359a2
Hyung Jip Moon <hyungjibi@gmail.com>
2015-05-01 19:09:11 -0700

    Removed server-ver2.js and added function that updateDB every 24hours
    Removed server-ver2.js and added function that updateDB every 24hours

--- server.js
+++ server.js
@@ -1,87 +1,141 @@
+//!!!!!!!!!!!!!
+//ASSUMPTION: Seasons lastUpdated is the indication of table or fixtures udpate
 
+//Retrieve existing DBs
 Leagues = new Mongo.Collection("leagues");
 Fixtures = new Mongo.Collection("fixtures");
 
 Meteor.startup(function () {
-
     if (Meteor.isServer){
-
+		
         //Leagues.remove({});
         //Fixtures.remove({});
 
-        // Update server every 24 hours to store the updated leagues and fixtures
+       // Update server every 24 hours to store the updated leagues and fixtures
         // 86400000 milliseconds = 24 Hours
-        setInterval(Meteor.call("storeLeague"), 86400000); 
-        setInterval(Meteor.call("storeFixture"), 86400000);
-        }
-    }
-);
+        setInterval(Meteor.call("updateDB"), 86400000); 
 
-  // API call for league
+		//Meteor.call("adminYak");
+    }
+});
 
 if (Meteor.isServer){
-
     Meteor.methods({
-
+        
+		//API call for league
         getLeague: function () {
+            return Meteor.http.call("GET", "http://api.football-data.org/alpha/soccerseasons", 
+                {headers: {"X-Auth-Token": "a87a67fb2885496bb1a5274e1eb79236"}, query: {"season": new Date().getFullYear()}});
 
-            return Meteor.http.call("GET", "http://api.football-data.org/alpha/soccerseasons/", {headers: {"X-Auth-Token": "a87a67fb2885496bb1a5274e1eb79236"}});
+            //TODO: should check the response status is 200. Otherwise try again. Timeout
+        },
+		
+		//API call for specific league's season worth fixtures
+        getFixtures: function (leagueId) {
+            return Meteor.http.call("GET", "http://api.football-data.org/alpha/soccerseasons/" + leagueId + "/fixtures/", {headers: {"X-Auth-Token": "a87a67fb2885496bb1a5274e1eb79236"}});
         },
 
-        getFixture: function () {
-
-            return Meteor.http.call("GET", "http://api.football-data.org/alpha/fixtures?timeFrame=n3", {headers: {"X-Auth-Token": "a87a67fb2885496bb1a5274e1eb79236"}});
-        },
-
-        storeLeague: function () {
-
-            var data = Meteor.call("getLeague");
+		//parsing data
+        parse: function(data) {
             var json = EJSON.stringify(data);
-            var obj = EJSON.parse(json);
-
-            for (i=0; i<obj.data.length; i++) {
-
-                 Leagues.insert({
-
-                    caption :  obj.data[i].caption,
-                    league  :   obj.data[i].league,                           
-                    year    :   obj.data[i].year,
-                    numberOfTeams   :   obj.data[i].numberOfTeams,
-                    numberOfGames   :   obj.data[i].numberOfGames,
-                    lastUpdated :   obj.data[i].lastUpdated,
-                    soccerseasons : obj.data[i]._links
-                 });
-            }            
+            return EJSON.parse(json);
         },
 
-        storeFixture: function () {
-
-            // var obj = JSON.parse(Meteor.call("getFixture"));
-
-            // for (i=0; i<fixtures.count; i++) {
-                // Fixtures.insert({
-                    
-                    id: 164374834
-                    // date: obj.fixtures[i].date,
-                    // status: obj.fixtures[i].status,
-                    // matchday: obj.fixtures[i].matchday,
-                    // homeTeamName: obj.fixtures[i].homeTeamName,
-                    // awayTeamName: obj.fixtures[i].awayTeamName,
-                    // goalsHomeTeam: obj.fixtures[i].goalsHomeTeam,
-                    // goalsAwayTeam: obj.fixtures[i].goalsAwayTeam,
-                    // FK_LEAGUES.leaguenumber:        
-                    // fixturenumber
-                //});
-            //}
+		//update entire mongoDB
+        updateDB: function(){
             
-            Fixtures.insert({
+            //update league
+            var leagueData = Meteor.call("getLeague");
+            var leagueJSON = Meteor.call("parse", leagueData);
+            var listOfUpdatedLeaguesId = Meteor.call("updateLeague", leagueJSON);
+            //Fixtures.insert(listOfUpdatedLeaguesId);
+            Meteor.call("updateFixture", listOfUpdatedLeaguesId);
+        },
 
-                 caption: "lalalalala"
-            });
-          
-        }        
-    });
-}
+		//update information about one league/competition in mongoDB 
+        updateLeague: function(league){  
+            var listOfUpdatedLeaguesNumber = [];
+            for (i=0; i<league.data.length; i++) {
+                listOfUpdatedLeaguesNumber.push(Meteor.call("storeLeague", league.data[i]));
+            }
+            return listOfUpdatedLeaguesNumber;
+        }, 
 
+		//update a season's worth of fixtures of a specific league
+        updateFixture: function(listOfLeagueIds){
 
+            //#of leagues
+            for (i=0; i<listOfLeagueIds.length; i++) {
+                
+                 var fixturesData = Meteor.call("getFixtures", listOfLeagueIds[i]);
+                 var fixturesJSON = Meteor.call("parse", fixturesData);
+                 
+                for(j=0; j<fixturesJSON.data.fixtures.length; j++){  
+                    Meteor.call("storeFixtures", fixturesJSON.data.fixtures[j], listOfLeagueIds[i]);
+                }
+            }
+        }, 
+		
+        //store data about one league/competition in mongoDB
+        storeLeague: function (league) {
 
+                var url = league._links.self.href;
+                var leagueNumber = url.substr(url.lastIndexOf('/') + 1);
+
+                if (Leagues.find({soccerseasons : {$eq: leagueNumber}}, {'soccerseasons' : 1}).count() > 0)
+                {
+                    Leagues.update({soccerseasons : {$eq: leagueNumber}}, {
+                        caption :  league.caption,
+                        league  :   league.league,                           
+                        year    :   league.year,
+                        numberOfTeams   :   league.numberOfTeams,
+                        numberOfGames   :   league.numberOfGames,
+                        lastUpdated :   league.lastUpdated,
+                        soccerseasons : leagueNumber
+                    });        
+                } else {
+                    Leagues.insert({
+                        caption :  league.caption,
+                        league  :   league.league,                           
+                        year    :   league.year,
+                        numberOfTeams   :   league.numberOfTeams,
+                        numberOfGames   :   league.numberOfGames,
+                        lastUpdated :   league.lastUpdated,
+                        soccerseasons : leagueNumber
+                    }); 
+                }  
+                return leagueNumber;        
+        },
+		
+		//store one fixture into DB
+        storeFixtures: function (singleFixture, leagueId) {
+
+                var uri = singleFixture._links.self.href;
+                var fixtureNumber = uri.substr(uri.lastIndexOf('/') + 1);
+                
+                if (Fixtures.find({fixtureId : {$eq: fixtureNumber}}, {'fixtureId': 1}).count()>0){
+                    Fixtures.update({fixtureId : {$eq: fixtureNumber}},{
+                        date: singleFixture.date,
+                        status: singleFixture.status,
+                        matchday: singleFixture.matchday,
+                        homeTeamName: singleFixture.homeTeamName,
+                        awayTeamName: singleFixture.awayTeamName,
+                        result: singleFixture.result,     
+                        fixtureId: fixtureNumber,
+                        leagueId: leagueId
+                    })
+                } else {
+                    Fixtures.insert({
+                	    date: singleFixture.date,
+                        status: singleFixture.status,
+                        matchday: singleFixture.matchday,
+                        homeTeamName: singleFixture.homeTeamName,
+                        awayTeamName: singleFixture.awayTeamName,
+                        result: singleFixture.result,     
+                        fixtureId: fixtureNumber,
+                        leagueId: leagueId
+                	});
+                }
+        	
+        }
+})}
