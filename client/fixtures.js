25f311863497455391d30b7d399a1fdb76467725
Michael Bae <michaelbaejunyoung@gmail.com>
2015-05-26 12:33:28 +0900

    preset the date range
    since there is no game during summer.
    better for dev work

--- client/fixtures.js
+++ client/fixtures.js
@@ -1,6 +1,9 @@
+// NO MATCH DURING SUMMER SO PRESET THE DATE
+// var p = new Date(new Date().setDate(new Date().getDate()-5)).toISOString();
+// var n = new Date(new Date().setDate(new Date().getDate()+5)).toISOString();
 
-var p = new Date(new Date().setDate(new Date().getDate()-5)).toISOString();
-var n = new Date(new Date().setDate(new Date().getDate()+5)).toISOString();
+var p = "2015-05-20T13:30:00Z";
+var n = "2015-05-30T13:30:00Z";
 
 Template.fixtures.helpers({
     fixtures: function() {
